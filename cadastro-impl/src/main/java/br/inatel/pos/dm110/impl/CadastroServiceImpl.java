d8de8bc60e358a969786737a310c3c18e7968cbe
Fernando Avellar <favellar@yahoo.com.br>
2017-11-13 07:25:54 -0200

    Apos finalizar trabalho

--- cadastro-impl/src/main/java/br/inatel/pos/dm110/impl/CadastroServiceImpl.java
+++ cadastro-impl/src/main/java/br/inatel/pos/dm110/impl/CadastroServiceImpl.java
@@ -3,10 +3,12 @@ package br.inatel.pos.dm110.impl;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
 
 import br.inatel.pos.dm110.api.CadastroService;
 import br.inatel.pos.dm110.interfaces.CadastroRemoto;
 
+@RequestScoped
 public class CadastroServiceImpl implements CadastroService {
 
 	@EJB(lookup="java:app/cadastro-ejb-0.1-SNAPSHOT/CadastroBean!br.inatel.pos.dm110.interfaces.CadastroRemoto")
